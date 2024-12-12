71082ffeeb6f9254b277e0355f6f806f07e86350
k023c0028 <k023c0028@g.neec.ac.jp>
2024-12-12 14:16:32 +0900

    update

---
+++ Game.java
@@ -0,0 +1,24 @@
+public class Game {
+    Player p;
+    Dice d;
+    
+    Game(){
+        p=new Player();
+        d=new Dice();
+    }
+
+    void start(){
+        p.turn(d);
+        System.out.println(d.me);
+        judge();
+    }
+    void judge(){
+        if (d.me==6){
+            System.out.println("当たり");
+        }
+        else{
+            System.out.println("外れ");
+        }
+    }
+
+}
---
+++ Player.java
@@ -0,0 +1,6 @@
+public class Player {
+    void turn(Dice d){//Diceを受け取る
+        d.roll();
+    }
+
+}
